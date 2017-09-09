752940b64002f0e2bede96169824788903a00a2a
tbabycheva <tetianababycheva@gmail.co>
2017-09-08 21:02:43 -0700

    Add custom colors

--- SaveTheSpot/Theme.swift
+++ SaveTheSpot/Theme.swift
@@ -7,3 +7,18 @@
 //
 
 import UIKit
+
+extension UIColor {
+    
+    class var asphaltGrey: UIColor {
+        return UIColor(red: 48.0/255.0, green: 57.0/255.0, blue: 64.0/255.0, alpha: 1.0)
+    }
+    
+    class var neonYellow: UIColor {
+        return UIColor(red: 241.0/255.0, green: 251.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    }
+    
+    class var oatmeal: UIColor {
+        return UIColor(red: 250.0/255.0, green: 245.0/255.0, blue: 240.0/255.0, alpha: 1.0)
+    }
+}
