10d4191c3b5322565a1e28519f9054f002f26d2d
tbabycheva <tetianababycheva@gmail.co>
2017-09-08 21:08:29 -0700

    Add presentation controller

--- SaveTheSpot/SlideInPresentationController..swift
+++ SaveTheSpot/SlideInPresentationController..swift
@@ -7,3 +7,98 @@
 //
 
 import UIKit
+
+
+final class SlideInPresentationController: UIPresentationController {
+    
+    // MARK: - Properties
+    fileprivate var dimmingView: UIView!
+    private var direction: PresentationDirection
+    
+    // update the frame of presented VC in addition to updating the size
+    override var frameOfPresentedViewInContainerView: CGRect {
+        var frame: CGRect = .zero
+        frame.size = size(forChildContentContainer: presentedViewController, withParentContainerSize: containerView!.bounds.size)
+        switch direction {
+        case .right:
+            frame.origin.x = containerView!.frame.width*(1.0/3.0)
+        case .bottom:
+            frame.origin.y = containerView!.frame.height*(1.0/3.0)
+        default:
+            frame.origin = .zero
+        }
+        return frame
+    }
+    
+    // MARK: - Initializers
+    init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?, direction: PresentationDirection) {
+        self.direction = direction
+        super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
+        setupDimmingView()
+    }
+    
+    // MARK: - Default Methods
+    
+    // show the dimming view
+    override func presentationTransitionWillBegin() {
+        containerView?.insertSubview(dimmingView, at: 0)
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "V:|[dimmingView]|", options: [], metrics: nil, views: ["dimmingView": dimmingView]))
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|[dimmingView]|", options: [], metrics: nil, views: ["dimmingView": dimmingView]))
+        
+        guard let coordinator = presentedViewController.transitionCoordinator else {
+            dimmingView.alpha = 1.0
+            return
+        }
+        
+        coordinator.animate(alongsideTransition: { _ in
+            self.dimmingView.alpha = 1.0
+        })
+    }
+    
+    // hide the dimming view
+    override func dismissalTransitionWillBegin() {
+        guard let coordinator = presentedViewController.transitionCoordinator else {
+            dimmingView.alpha = 0.0
+            return
+        }
+        
+        coordinator.animate(alongsideTransition: { _ in
+            self.dimmingView.alpha = 0.0
+        })
+    }
+    
+    // respond to layout changes in presentation VC
+    override func containerViewWillLayoutSubviews() {
+        presentedView?.frame = frameOfPresentedViewInContainerView
+    }
+    
+    // set size to 2/3 of the screen (also update the frame to 2/3)
+    override func size(forChildContentContainer container: UIContentContainer, withParentContainerSize parentSize: CGSize) -> CGSize {
+        switch direction {
+        case .left, .right:
+            return CGSize(width: parentSize.width*(2.0/3.0), height: parentSize.height)
+        case .bottom, .top:
+            return CGSize(width: parentSize.width, height: parentSize.height*(2.0/3.0))
+        }
+    }
+}
+
+// MARK: - Private
+private extension SlideInPresentationController {
+    
+    // set up background as a dimmed view
+    func setupDimmingView() {
+        dimmingView = UIView()
+        dimmingView.translatesAutoresizingMaskIntoConstraints = false
+        dimmingView.backgroundColor = UIColor(white: 0.0, alpha: 0.5)
+        dimmingView.alpha = 0.0
+        
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
+        dimmingView.addGestureRecognizer(recognizer)
+    }
+    
+    // dismiss modal when tapped on a dimmed view
+    dynamic func handleTap(recognizer: UITapGestureRecognizer) {
+        presentingViewController.dismiss(animated: true)
+    }
+}
