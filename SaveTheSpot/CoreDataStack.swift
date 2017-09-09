d2623e7ca427ea07b295209facd2344bb47f1d79
tbabycheva <tetianababycheva@gmail.co>
2017-09-08 20:20:43 -0700

    Add 'spot' model object

--- SaveTheSpot/CoreDataStack.swift
+++ SaveTheSpot/CoreDataStack.swift
@@ -8,3 +8,19 @@
 
 import Foundation
 import CoreData
+
+class CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "SaveTheSpot")
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext { return container.viewContext }
+}
--- SaveTheSpot/Spot+Convenience.swift
+++ SaveTheSpot/Spot+Convenience.swift
@@ -10,3 +10,29 @@ import Foundation
 import CoreData
 import MapKit
 
+extension SpotMO {
+    
+    convenience init(name: String,
+                     address: String,
+                     placemark: MKPlacemark,
+                     context: NSManagedObjectContext? = CoreDataStack.context) {
+        if let context = context {
+            // This is the object that is saved to Core Data
+            self.init(context: context)
+        } else {
+            // This is a temporary model object in memory
+            self.init(entity: SpotMO.entity(), insertInto: nil)
+        }
+        
+        self.name = name
+        self.address = address
+        self.latitude = placemark.coordinate.latitude
+        self.longitude = placemark.coordinate.longitude
+    }
+    
+    var placemark: MKPlacemark  {
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let placemark = MKPlacemark(coordinate: coordinate)
+        return placemark
+    }
+}
