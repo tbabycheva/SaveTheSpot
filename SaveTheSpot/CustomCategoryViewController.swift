d59478ec4e6f98bd40cd8678058f74a79d8f5057
tbabycheva <tetianababycheva@gmail.co>
2017-09-08 21:48:09 -0700

    Set up properties and configure viewDidLoad

--- SaveTheSpot/CustomCategoryViewController.swift
+++ SaveTheSpot/CustomCategoryViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol CustomCategoryViewControllerDelegate: class {
+    func saveCategoryWasTapped()
+}
+
 class CustomCategoryViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: - IBOutlets
@@ -16,7 +20,28 @@ class CustomCategoryViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var customCategoryCollectionView: UICollectionView!
     @IBOutlet weak var tagName: UITextField!
     
+    // MARK: - Properties
     
+    var category: CategoryMO?
+    var iconName: String = ""
+    let limitLength = 25
+    
+    weak var delegate: CustomCategoryViewControllerDelegate?
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        customCategoryCollectionView.dataSource = self
+        customCategoryCollectionView.delegate = self
+        
+        modalView.layer.cornerRadius = 15.0
+        self.view.backgroundColor = UIColor.init(white: 0.0, alpha: 0.75)
+        tagName.delegate = self
+        
+        CategoryController.shared.availableIcons = CategoryController.shared.loadFromUserDefaults()
+    }
     
     @IBAction func dismissButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
