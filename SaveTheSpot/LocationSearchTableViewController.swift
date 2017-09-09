83d2512ba14f823b2131bb7b0f3c8dac54413af6
tbabycheva <tetianababycheva@gmail.co>
2017-09-08 21:22:03 -0700

    Set up the API call for local search

--- SaveTheSpot/LocationSearchTableViewController.swift
+++ SaveTheSpot/LocationSearchTableViewController.swift
@@ -14,3 +14,27 @@ class LocationSearchTableViewController: UITableViewController {
     var matchingItems:[MKMapItem] = []
     var mapView: MKMapView?
 }
+
+// MARK: - Set up the API call
+
+extension LocationSearchTableViewController : UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let mapView = mapView,
+            let searchBarText = searchController.searchBar.text else { return }
+        
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBarText
+        request.region = mapView.region
+        
+        let search = MKLocalSearch(request: request)
+        
+        search.start { response, _ in
+            guard let response = response else { return }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
+    }
+}
+
