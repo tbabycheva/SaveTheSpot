10c626d7dc33089838aa8bfce9c19fbdfdbc06d3
tbabycheva <tetianababycheva@gmail.co>
2017-09-08 21:59:14 -0700

    Add sections with category names as titles and rows with spots

--- SaveTheSpot/CategoriesViewController.swift
+++ SaveTheSpot/CategoriesViewController.swift
@@ -69,3 +69,40 @@ class CategoriesViewController: UIViewController {
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
 }
+
+extension CategoriesViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return CategoryController.shared.allCategoriesUsedBySpotsSorted.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return CategoryController.shared.categoryNames[section]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let category = CategoryController.shared.allCategoriesUsedBySpotsSorted[section]
+        let spots = SpotController.shared.spotsWith(category: category)
+        return spots.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath) as? CategoryTableViewCell else { return CategoryTableViewCell() }
+        
+        let category = CategoryController.shared.allCategoriesUsedBySpotsSorted[indexPath.section]
+        let spots = SpotController.shared.spotsWith(category: category)
+        let spotNames = spots.flatMap { $0.name }
+        
+        cell.nameLabel?.text = spotNames[indexPath.row]
+        if let iconName = category.iconName {
+            cell.iconImageView.image = UIImage(named: "\(iconName)")
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+}
+
+
