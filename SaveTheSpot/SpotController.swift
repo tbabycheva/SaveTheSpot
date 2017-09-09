8c82df29ce9242f433fa975bbae7baeb52cbd448
tbabycheva <tetianababycheva@gmail.co>
2017-09-08 20:25:13 -0700

    Initialize 'spots'

--- SaveTheSpot/SpotController.swift
+++ SaveTheSpot/SpotController.swift
@@ -9,3 +9,52 @@
 import Foundation
 import MapKit
 import CoreData
+
+class SpotController {
+    
+    static let shared = SpotController()
+    
+    static let spotsWereUpdatedNotification = Notification.Name("spotsWereUpdated")
+    
+    var spots: [SpotMO] {
+        
+        let fetchRequest: NSFetchRequest<SpotMO> = SpotMO.fetchRequest()
+        
+        do {
+            let results = try CoreDataStack.context.fetch(fetchRequest)
+            DispatchQueue.main.async {
+                NotificationCenter.default.post(name: SpotController.spotsWereUpdatedNotification, object: self)
+            }
+            return results.sorted(by: {
+                guard let firstSpotName = $0.name else { return false }
+                guard let secondSpotName = $1.name else { return true }
+                return firstSpotName < secondSpotName }
+            )
+        } catch {
+            print("Error fetching SpotMO from persistentStore")
+            return []
+        }
+    }
+    
+    var spotsToDisplay: [SpotMO] = [] {
+        didSet {
+        }
+    }
+    
+    init() {
+        spotsToDisplay = spots
+    }
+}
+
+// CoreData
+
+extension SpotController {
+    
+    func saveToPersistentStore() {
+        do {
+            try CoreDataStack.context.save()
+        } catch {
+            NSLog("There was an error saving: \(error)")
+        }
+    }
+}
